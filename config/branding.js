fedb0ed1e5955ad5731824e2bba7722e7cc98c93
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-07-07 11:11:01 -0500

    add icons

---
+++ config/branding.js
@@ -0,0 +1,18 @@
+exports.assets = {
+  fabric8: {
+    favicon: {
+      path: './src/assets/icon/fabric8_icon.svg'
+    },
+    title: {
+      prefix: 'OpenfactSync'
+    }
+  },
+  openshiftio: {
+    favicon: {
+      path: './src/assets/icon/openshift_icon.svg'
+    },
+    title: {
+      prefix: 'OpenfactSync.io'
+    }
+  }
+}
