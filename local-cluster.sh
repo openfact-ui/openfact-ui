b97d6f5d68fa095c9589e5300249425f2761f459
Carlos Feria <carlosthe19916@gmail.com>
2017-11-08 18:14:50 -0500

    change api location

--- local-cluster.sh
+++ local-cluster.sh
@@ -5,7 +5,7 @@ oc project openfact-sync-system
 export OPENFACT_REALM="openfact"
 
 export OPENFACT_SSO_API_URL="http://`oc get route keycloak --template={{.spec.host}}`/"
-export OPENFACT_SYNC_API_URL="http://`oc get route sync --template={{.spec.host}}`/api/"
+export OPENFACT_SYNC_API_URL="http://`oc get route openfact-sync --template={{.spec.host}}`/api/"
 
 echo ""
 echo "OPENFACT_SSO_API_URL           ${OPENFACT_SSO_API_URL}"
