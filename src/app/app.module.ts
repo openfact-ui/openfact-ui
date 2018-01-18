eb17f7c8fbd28384ba6658fa3aac3d43fcc4a0dd
Carlos Feria <carlosthe19916@gmail.com>
2018-01-18 15:00:15 -0500

    add keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { XHRBackend, RequestOptions } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+import { KeycloakService } from './keycloak/keycloak.service';
+import { KeycloakHttp, keycloakHttpFactory } from './keycloak/keycloak.http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: KeycloakHttp,
+      useFactory: keycloakHttpFactory,
+      deps: [XHRBackend, RequestOptions, KeycloakService]
+    },
+    KeycloakService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  keycloakRootUrl: 'http://keycloak-keycloak-sso.apps.console.sistcoop.org/auth'
 };
