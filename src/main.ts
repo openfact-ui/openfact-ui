1478346ba420749061575dbd47a16c6c8550f184
Carlos Feria <carlosthe19916@gmail.com>
2018-01-19 10:28:30 -0500

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { KeycloakService } from './app/keycloak-service/keycloak.service';
-import { window } from 'rxjs/operators/window';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const noLogin: boolean = false; // convenient for development
-if (noLogin) {
-  platformBrowserDynamic().bootstrapModule(AppModule);
-} else {
-  KeycloakService.init({ onLoad: 'login-required' }).then(() => {
-    platformBrowserDynamic().bootstrapModule(AppModule)
-  }).catch((err: any) => {
-    console.log('Error in bootstrap: ' + JSON.stringify(err));
-  });
-}
+KeycloakService.init({ onLoad: 'login-required' }).then(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+}).catch((err: any) => {
+  console.log('Error in bootstrap: ' + JSON.stringify(err));
+});
+
