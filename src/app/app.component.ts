10bdc0937a4540aaa6ccd707a205f44326c34c7f
Carlos Feria <carlosthe19916@gmail.com>
2018-01-23 16:44:21 -0500

    add sass precompilers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cn-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'cn';
