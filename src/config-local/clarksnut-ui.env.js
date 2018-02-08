003d55563c85aaecfc1d7425810cb43fb4d4e897
Carlos Feria <carlosthe19916@gmail.com>
2018-02-08 18:12:58 -0500

    add openshift configuration

---
+++ src/config-local/clarksnut-ui.env.js
@@ -0,0 +1,6 @@
+window.ClarksnutUIEnv = {
+  "ssoApiUrl": "http://localhost:8180/auth",
+  "clarksnutApiUrl": "http://localhost:8080/api",
+  "clarksnutMailCollectorApiUrl": "http://localhost:8081/api",
+  "authApiUrl": "http://localhost:8080/api"
+};
--- src/config/clarksnut-ui.env.js
+++ src/config/clarksnut-ui.env.js
@@ -1,20 +1,6 @@
-/*window.ClarksnutUIEnv = {
+window.ClarksnutUIEnv = {
   "ssoApiUrl": "{{ .Env.CLARKSNUT_SSO_API_URL }}",
   "clarksnutApiUrl": "{{ .Env.CLARKSNUT_API_URL }}",
   "clarksnutMailCollectorApiUrl": "{{ .Env.CLARKSNUT_MAIL_COLLECTOR_API_URL }}",
   "authApiUrl": "{{ .Env.CLARKSNUT_AUTH_API_URL }}",
-};*/
-
-/*window.ClarksnutUIEnv = {
-  "ssoApiUrl": "http://localhost:8180/auth",
-  "clarksnutApiUrl": "http://localhost:8080/api",
-  "clarksnutMailCollectorApiUrl": "http://localhost:8081/api",
-  "authApiUrl": "http://localhost:8080/api"
-};*/
-
-window.ClarksnutUIEnv = {
-  "ssoApiUrl": "http://keycloak-keycloak-sso.apps.console.sistcoop.org/auth",
-  "clarksnutApiUrl": "http://clarksnut-clarksnut.apps.console.sistcoop.org/api",
-  "clarksnutMailCollectorApiUrl": "http://clarksnut-mail-collector-clarksnut.apps.console.sistcoop.org/api",
-  "authApiUrl": "http://clarksnut-clarksnut.apps.console.sistcoop.org/api"
 };
