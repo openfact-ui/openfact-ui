c876744ec9e6bcd5ee838aef87538f1882a58456
Carlos Feria <carlosthe19916@gmail.com>
2018-03-12 09:44:22 -0500

    add settings

--- src/app/space/edit/settings/settings.component.ts
+++ src/app/space/edit/settings/settings.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Notifications, Notification, NotificationType } from './../../../ngx/ngx-base';
+import { Context, Space, SpaceService, SpaceAttributes, SpaceRelatedLink } from './../../../ngx/ngx-clarksnut';
+import { ContextService } from './../../../ngx-impl/ngx-clarksnut-impl/context.service';
+
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'cn-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  space: Space;
+
+  private _context: Context;
+  private subcriptions: Subscription[] = [];
+
+  constructor(
+    private spaceService: SpaceService,
+    private contexts: ContextService,
+    private notifications: Notifications
+  ) {
+    this.subcriptions.push(
+      contexts.current.subscribe((context) => {
+        this._context = context
+        this.spaceService.getUserSpaceById('me', context.space.id).subscribe((space) => {
+          this.space = space;
+        });
+      })
+    );
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subcriptions.forEach((subs) => subs.unsubscribe());
+  }
+
+  save() {
+    const updatedSpace = this.createTransientSpace();
+    updatedSpace.attributes.name = this.space.attributes.name;
+    updatedSpace.attributes.description = this.space.attributes.description;
+
+    this.spaceService.update('me', updatedSpace).subscribe((val) => {
+      this.notifications.message({
+        message: 'Espacio actualizado',
+        type: NotificationType.SUCCESS
+      } as Notification);
+    });
+  }
+
+  createTransientSpace(): Space {
+    const space = {} as Space;
+    space.id = this.space.id;
+    space.type = 'spaces';
+    space.attributes = new SpaceAttributes();
+    return space;
   }
 
 }
--- src/app/space/edit/settings/settings.module.ts
+++ src/app/space/edit/settings/settings.module.ts
@@ -1,12 +1,14 @@
-import { SettingsComponent } from './settings.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsComponent } from './settings.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    FormsModule,
   ],
   declarations: [
     SettingsComponent
