eed2debe7584a448a7b36cd2595adf7a7c7c863e
Carlos Feria <carlosthe19916@gmail.com>
2018-03-19 16:08:15 -0500

    changes

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-ng build --prod --aot --build-optimizer
-docker build -t clarksnut/clarksnut-ui -f Dockerfile.deploy .
+npm build --prod --aot --build-optimizer
+docker build -t clarksnut/clarksnut-ui .;
+docker build -t clarksnut/clarksnut-ui-openshift -f Dockerfile.deploy .
+docker tag clarksnut/clarksnut-ui clarksnut/clarksnut-ui:$(git rev-parse --short HEAD);
+docker tag clarksnut/clarksnut-ui-openshift clarksnut/clarksnut-ui-openshift:$(git rev-parse --short HEAD);
 docker push clarksnut/clarksnut-ui
