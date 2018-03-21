3fa095327abd3a134bb289550d53be458754ab54
Carlos Feria <carlosthe19916@gmail.com>
2018-03-21 09:15:48 -0500

    add docker automation

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-npm build --prod --aot --build-optimizer
-docker build -t clarksnut/clarksnut-ui .;
+ng build --prod --aot --build-optimizer
+docker build -t clarksnut/clarksnut-ui .
 docker build -t clarksnut/clarksnut-ui-openshift -f Dockerfile.deploy .
 docker tag clarksnut/clarksnut-ui clarksnut/clarksnut-ui:$(git rev-parse --short HEAD);
 docker tag clarksnut/clarksnut-ui-openshift clarksnut/clarksnut-ui-openshift:$(git rev-parse --short HEAD);
 docker push clarksnut/clarksnut-ui
+docker push clarksnut/clarksnut-ui-openshift
